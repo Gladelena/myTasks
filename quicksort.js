05957ad4a1cb3e805bdfae4e7024cc8250e52a62
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-06-20 18:23:17 +0300

    quicksort JS

---
+++ quicksort.js
@@ -0,0 +1,31 @@
+"user script";
+arr = [4, 15, -7, 5, 3, 6, -5, 6, 0, 2, 1];
+function quickSort(arr, left, right) {
+   var mid = arr[Math.floor((right + left) / 2)];
+   var i = left;
+   var j = right;
+   while (i < j) {
+      while (arr[i] < mid) {
+         i++;
+      };
+      while (arr[j] > mid) {
+         j--;
+      };
+      if (i <= j) {
+         var tmp=arr[i];
+         arr[i]=arr[j];
+         arr[j]=tmp;
+         i++;
+         j--;
+      };
+   };
+   if (left<j){
+      quickSort(arr, left, j);
+   };
+   if (i<right){
+      quickSort(arr, i, right)
+   };
+   return arr;
+};
+var result = quickSort(arr, 0, arr.length-1);
+console.log(result);

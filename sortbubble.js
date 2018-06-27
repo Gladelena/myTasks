23a6cedf745efa217b9a6fad95b3e89c29f12002
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-06-27 21:17:15 +0300

    Update sortbubble.js

--- sortbubble.js
+++ sortbubble.js
@@ -1,14 +1,14 @@
-arr=[7,2,3,4,5,1,9];
-var ln=arr.length;
-var tmp;
-console.log("Старый массив " +arr);
-for (j=0; j<ln; j++){
-	for (var i=0; i<ln-1; i++){
-		if (arr[i]>arr[i+1]) {
-			tmp=arr[i];
-			arr[i]=arr[i+1];
-			arr[i+1]=tmp;
-		}
-	}
-}	
+"user scrict";
+arr = [7, 2, 3, 4, 5, 1, 9];
+var ln = arr.length;
+console.log("Старый массив " + arr);
+for (var i = 0; i < ln-1; i++) {
+   for (var j = 0; j < ln-1-i; j++) {
+      if (arr[j] > arr[j+1]) {
+         var tmp = arr[j];
+         arr[j] = arr[j+1];
+         arr[j+1] = tmp;
+      }
+   }
+};	
 console.log("Новый массив " + arr);

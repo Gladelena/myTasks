363724b49aab27cb1183a59558c6efd844923a10
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-01 18:43:23 +0300

    Update Lamp.js

--- SmartHouse/Lamp.js
+++ SmartHouse/Lamp.js
@@ -3,40 +3,31 @@ function Lamp(name) {
    Device.call(this, "lamp", name);
    this.MIN_VALUE_POWER = 0;
    this.MAX_VALUE_POWER = 100;
-   this._power = this.MIN_VALUE_POWER;
+   this._currentLight = 50;
 }
 
    Lamp.prototype = Object.create(Device.prototype);
    Lamp.prototype.constructor = Lamp;
 
    Lamp.prototype.getPower = function() {
-      return this._power;
-   };
-   Lamp.prototype.setPower = function(value) {
-      if (value < this.MIN_VALUE_POWER || value > this.MAX_VALUE_POWER || (value % 5)){
-         throw new RangeError("Incorrect value power. Value power must be multiple of five and in range from " + this.MIN_VALUE_POWER + " to " + this.MAX_VALUE_POWER);
-      } else {
-         this._power = value;
-      }
+      return this._currentLight;
    };
    Lamp.prototype.plusPower = function() {
-      if (this._power < this.MAX_VALUE_POWER) {
-         this._power += 5;
-      } else {
-         this.isPowered();
-      }
+      if (this._currentLight < this.MAX_VALUE_POWER) {
+         this._currentLight++;
+      } 
    };
    Lamp.prototype.minusPower = function() {
-      if(this._power > this.MIN_VALUE_POWER) {
-         this._power -= 5;
+      if(this._currentLight > this.MIN_VALUE_POWER) {
+         this._currentLight--;
       } else {
-         this.isPowered();
+      this.isPowered();
       }
    };
    Lamp.prototype.isPowered = function() {
-      if (this._power == this.MIN_VALUE_POWER) {
-         this.off();
-      } else {
+      if (this._currentLight != this.MIN_VALUE_POWER) {
          this.on();
+      } else {
+         this.off();
       }
    };

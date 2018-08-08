fd108897204bbed70a8929d813cf1981c01f716d
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-08 23:46:58 +0300

    Update Lamp.js

--- SmartHouse/Lamp.js
+++ SmartHouse/Lamp.js
@@ -3,29 +3,35 @@ function Lamp(name) {
    Device.call(this, "lamp", name);
    this.MIN_VALUE_POWER = 0;
    this.MAX_VALUE_POWER = 100;
-   this._currentLight = 50;
+   this._power = this.MIN_VALUE_POWER;
 }
 
    Lamp.prototype = Object.create(Device.prototype);
    Lamp.prototype.constructor = Lamp;
 
    Lamp.prototype.getPower = function() {
-      return this._currentLight;
+      return this._power;
    };
+     Lamp.prototype.setPower = function(value){
+    if (value>=this.MIN_VALUE_POWER && value<=this.MAX_VALUE_POWER) {
+      this._power = value;
+    }
+    this.isPowered();
+  };
    Lamp.prototype.plusPower = function() {
-      if (this._currentLight < this.MAX_VALUE_POWER) {
-         this._currentLight++;
+      if (this._power < this.MAX_VALUE_POWER) {
+         this._power++;
       } 
    };
    Lamp.prototype.minusPower = function() {
-      if(this._currentLight > this.MIN_VALUE_POWER) {
-         this._currentLight--;
+      if(this._power > this.MIN_VALUE_POWER) {
+         this._power--;
       } else {
       this.isPowered();
       }
    };
    Lamp.prototype.isPowered = function() {
-      if (this._currentLight != this.MIN_VALUE_POWER) {
+      if (this._power != this.MIN_VALUE_POWER) {
          this.on();
       } else {
          this.off();

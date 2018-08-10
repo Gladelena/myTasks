b5d0274192ef1116366ba00ea2a5bf8b26dfaec8
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-10 13:22:31 +0300

    Update TV.js

--- SmartHouse/TV.js
+++ SmartHouse/TV.js
@@ -3,7 +3,7 @@ function TV(name) {
    Device.call(this, "tv", name);
    this._name = name;
    this.MIN_CHANNEL_NUMBER = 1;
-   this.MAX_CHANNEL_NUMBER = 999;
+   this.MAX_CHANNEL_NUMBER = 200;
    this.MIN_VOLUME = 0;
    this.MAX_VOLUME = 100;
    this.channelNumber = this.MIN_CHANNEL_NUMBER;
@@ -24,12 +24,12 @@ function TV(name) {
       }
    };
    TV.prototype.plusChannel = function() {
-      if (this.channelNumber >= MIN_CHANNEL_NUMBER && this.channelNumber < MAX_CHANNEL_NUMBER) {
+      if (this.channelNumber >= this.MIN_CHANNEL_NUMBER && this.channelNumber < this.MAX_CHANNEL_NUMBER) {
          this.channelNumber++;
       }
    };
    TV.prototype.minusChannel = function() {
-      if(this.channelNumber > MIN_CHANNEL_NUMBER) {
+      if(this.channelNumber > this.MIN_CHANNEL_NUMBER) {
          this.channelNumber--;
       }
    };

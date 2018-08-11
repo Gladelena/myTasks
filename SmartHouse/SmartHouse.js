80791a2123e9ba2d485ca8038dadb7a586d960ae
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 18:20:29 +0300

    Update SmartHouse.js

--- SmartHouse/SmartHouse.js
+++ SmartHouse/SmartHouse.js
@@ -6,21 +6,9 @@ function SmartHouse() {
       lamp: []
    };
 }
-
-   SmartHouse.prototype.getDeviceByName = function(type, name) {
-      var array = this._devices[type];
-      for (var i in array) {
-         if (array[i].getName() == name) {
-            return array[i];
-         } else {
-            return "there is no such device";
-         }
-      }
-   };
-
-   SmartHouse.prototype.addDevice = function(type, name) {
-      if (!this._devices[type].includes(this.getDeviceByName(type, name))) { 
-         switch(type) {
+   SmartHouse.prototype.addDevice = function (type, name) {
+      if(!this._devices[type].includes(this.getDeviceByName(type, name))){ 
+         switch (type) {
             case "tv":
                this._devices.tv.push(new TV(name));
                break;
@@ -34,6 +22,20 @@ function SmartHouse() {
       } else {
          throw new TypeError("Name of device already exists");
       }
+   };
+   SmartHouse.prototype.addDevices = function (device) {
+      if (this._devices.hasOwnProperty(device.getType())) {
+         this._devices[device.getType()].push(device);
+      }
+   };
+   SmartHouse.prototype.getDeviceByName = function(type, name){
+      var array = this._devices[type];
+      for (var i in array) {
+         if (array[i].getName()==name) {
+            return array[i];
+         } 
+      }
+      return "there is no such device";
    };
 
    SmartHouse.prototype.deleteDeviceByName = function(type, name) {

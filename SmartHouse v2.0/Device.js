df3570a53681360faf198979b4ef8933558443ee
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 23:24:59 +0300

    Update Device.js

--- SmartHouse v2.0/Device.js
+++ SmartHouse v2.0/Device.js
@@ -1,25 +1,25 @@
 "use strict";
 class Device {
    constructor (type, name){
-   this._type = type;
-   this._name = name;
-   this._isOn = false;
-}
+      this._type = type;
+      this._name = name;
+      this._isOn = false;
+   }
    on() {
       this._isOn = true;
-   };
+   }
    off() {
       this._isOn = false;
    }
    get(){
       return this._name;
-   };
+   }
    set(name){
       this.name = name;
-   };
+   }
    get(){
       return this._type;
-   };
+   }
 }
 
 

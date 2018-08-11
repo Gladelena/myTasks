71427fc1f587dc2f2fa4758c6541c5afb93e2dcc
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 23:28:14 +0300

    Update Lamp.js

--- SmartHouse v2.0/Lamp.js
+++ SmartHouse v2.0/Lamp.js
@@ -9,30 +9,30 @@ class Lamp extends Device{
 
    get() {
       return this._power;
-   };
+   }
    set(value){
       if (value>=Lamp.MIN_VALUE_POWER && value<=Lamp.MAX_VALUE_POWER) {
          this._power = value;
       }
       this.isPowered();
-   };
+   }
    plusPower() {
       if (this._power < Lamp.MAX_VALUE_POWER) {
          this._power++;
       } 
-   };
+   }
    minusPower() {
       if(this._power > Lamp.MIN_VALUE_POWER) {
          this._power--;
       } else {
       this.isPowered();
       }
-   };
+   }
    isPowered() {
       if (this._power != Lamp.MIN_VALUE_POWER) {
          this.on();
       } else {
          this.off();
       }
-   };
+   }
 }

a607a76bcaa674890841737a5f548aae71cbe00a
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 20:03:05 +0300

    Update TV.js

--- SmartHouse/TV.js
+++ SmartHouse/TV.js
@@ -2,14 +2,15 @@
 function TV(name) {
    Device.call(this, "tv", name);
    this._name = name;
-   this.MIN_CHANNEL_NUMBER = 1;
-   this.MAX_CHANNEL_NUMBER = 200;
-   this.MIN_VOLUME = 0;
-   this.MAX_VOLUME = 100;
-   this.channelNumber = this.MIN_CHANNEL_NUMBER;
+   this.channelNumber = TV.MIN_CHANNEL_NUMBER;
    this.volume = 15;
 }
 
+   TV.MIN_CHANNEL_NUMBER = 1;
+   TV.MAX_CHANNEL_NUMBER = 200;
+   TV.MIN_VOLUME = 0;
+   TV.MAX_VOLUME = 50;
+   
    TV.prototype = Object.create(Device.prototype);
    TV.prototype.constructor = TV;
 
@@ -17,19 +18,19 @@ function TV(name) {
       return this.channelNumber;
    };
    TV.prototype.setChannel = function(channelNumber) {
-      if (channelNumber < this.MIN_CHANNEL_NUMBER || channelNumber > this.MAX_CHANNEL_NUMBER) {
+      if (channelNumber < TV.MIN_CHANNEL_NUMBER || channelNumber > TV.MAX_CHANNEL_NUMBER) {
          throw new RangeError("Incorrect channel number");
       } else {
          this.channelNumber = channelNumber;
       }
    };
    TV.prototype.plusChannel = function() {
-      if (this.channelNumber >= this.MIN_CHANNEL_NUMBER && this.channelNumber < this.MAX_CHANNEL_NUMBER) {
+      if (this.channelNumber >= TV.MIN_CHANNEL_NUMBER && this.channelNumber < TV.MAX_CHANNEL_NUMBER) {
          this.channelNumber++;
       }
    };
    TV.prototype.minusChannel = function() {
-      if(this.channelNumber > this.MIN_CHANNEL_NUMBER) {
+      if(this.channelNumber > TV.MIN_CHANNEL_NUMBER) {
          this.channelNumber--;
       }
    };
@@ -37,12 +38,12 @@ function TV(name) {
       return this.volume;
    };
    TV.prototype.plusVolume = function() {
-      if(this.volume < this.MAX_VOLUME) {
+      if(this.volume < TV.MAX_VOLUME) {
          this.volume++;
       }
    };
    TV.prototype.minusVolume = function() {
-      if(this.volume > this.MIN_VOLUME) {
+      if(this.volume > TV.MIN_VOLUME) {
          this.volume--;
       }
    };

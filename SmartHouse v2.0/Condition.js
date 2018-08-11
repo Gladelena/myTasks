5bb3e7e2d3b3ceda34d8126fc33b7229e84af8ad
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 23:24:15 +0300

    Update Condition.js

--- SmartHouse v2.0/Condition.js
+++ SmartHouse v2.0/Condition.js
@@ -1,28 +1,27 @@
 "use strict";
 class Condition extends Device{
    constructor(name){
-   super ("condition", name);
-   Condition.MAX_VALUE_TEMPER = 30;
-   Condition.MIN_VALUE_TEMPER = 15;
-   this._temperature = 23;
-   this._modes = ["cool", "heat", "auto"];
-   this._currentMode = this._modes[2];
+      super ("condition", name);
+      Condition.MAX_VALUE_TEMPER = 30;
+      Condition.MIN_VALUE_TEMPER = 15;
+      this._temperature = 23;
+      this._modes = ["cool", "heat", "auto"];
+      this._currentMode = this._modes[2];
    }
-   
-   
+     
    plusTemperature() {
       if (this._temperature < Condition.MAX_VALUE_TEMPER) {
          this._temperature++;
       }
-   };
+   }
    minusTemperature() {
       if (this._temperature > Condition.MIN_VALUE_TEMPER){
          this._temperature--;
       }
-   };
+   }
    get(){
       return this._temperature;
-   };
+   }
    set(value) {
       if (this._modes.includes(value)) {
          this._currentMode = value;
@@ -33,8 +32,8 @@ class Condition extends Device{
       if (this._currentMode == "heat") {
          this._temperature = 26;
       }
-   };
+   }
    get() {
       return this._currentMode;
-   };
+   }
 }

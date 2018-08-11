c2ffa5dc08509ed09a8fab3cc9325031394c8245
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 23:29:13 +0300

    Update TV.js

--- SmartHouse v2.0/TV.js
+++ SmartHouse v2.0/TV.js
@@ -13,35 +13,35 @@ class TV extends Device{
 
    get() {
       return this.channelNumber;
-   };
+   }
    set(channelNumber) {
       if (channelNumber < TV.MIN_CHANNEL_NUMBER || channelNumber > TV.MAX_CHANNEL_NUMBER) {
          throw new RangeError("Incorrect channel number");
       } else {
          this.channelNumber = channelNumber;
       }
-   };
+   }
    plusChannel() {
       if (this.channelNumber >= TV.MIN_CHANNEL_NUMBER && this.channelNumber < TV.MAX_CHANNEL_NUMBER) {
          this.channelNumber++;
       }
-   };
+   }
    minusChannel() {
       if(this.channelNumber > TV.MIN_CHANNEL_NUMBER) {
          this.channelNumber--;
       }
-   };
+   }
    get() {
       return this.volume;
-   };
+   }
    plusVolume() {
       if(this.volume < TV.MAX_VOLUME) {
          this.volume++;
       }
-   };
+   }
    minusVolume() {
       if(this.volume > TV.MIN_VOLUME) {
          this.volume--;
       }
-   };
+   }
 }

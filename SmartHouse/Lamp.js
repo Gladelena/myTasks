94af03fce2c6b983d3a6c2b54e1e1ab63297390a
Gladelena <32564157+Gladelena@users.noreply.github.com>
2018-08-11 19:44:16 +0300

    Update Lamp.js

--- SmartHouse/Lamp.js
+++ SmartHouse/Lamp.js
@@ -1,11 +1,10 @@
 "use strict";
 function Lamp(name) {
    Device.call(this, "lamp", name);
-   this.MIN_VALUE_POWER = 0;
-   this.MAX_VALUE_POWER = 100;
-   this._power = this.MIN_VALUE_POWER;
+   this._power = Lamp.MIN_VALUE_POWER;
 }
-
+   Lamp.MIN_VALUE_POWER = 0;
+   Lamp.MAX_VALUE_POWER = 100;
    Lamp.prototype = Object.create(Device.prototype);
    Lamp.prototype.constructor = Lamp;
 
@@ -13,25 +12,25 @@ function Lamp(name) {
       return this._power;
    };
      Lamp.prototype.setPower = function(value){
-    if (value>=this.MIN_VALUE_POWER && value<=this.MAX_VALUE_POWER) {
+    if (value>=Lamp.MIN_VALUE_POWER && value<=Lamp.MAX_VALUE_POWER) {
       this._power = value;
     }
     this.isPowered();
   };
    Lamp.prototype.plusPower = function() {
-      if (this._power < this.MAX_VALUE_POWER) {
+      if (this._power < Lamp.MAX_VALUE_POWER) {
          this._power++;
       } 
    };
    Lamp.prototype.minusPower = function() {
-      if(this._power > this.MIN_VALUE_POWER) {
+      if(this._power > Lamp.MIN_VALUE_POWER) {
          this._power--;
       } else {
       this.isPowered();
       }
    };
    Lamp.prototype.isPowered = function() {
-      if (this._power != this.MIN_VALUE_POWER) {
+      if (this._power != Lamp.MIN_VALUE_POWER) {
          this.on();
       } else {
          this.off();
